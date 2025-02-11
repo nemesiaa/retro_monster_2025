6fe35285195cd0f46b2879ad50d9da6e15c0a37d
Nemesiaa <gillot.nina@gmail.com>
2025-02-11 13:26:13 +0100

    PagesController: homeAction  + ajout des images

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
 
-Route::get('/', function () {
-    return view('templates/index');
-});
+
+Route::get('/', [PagesController::class, 'homeAction'])->name('home');
